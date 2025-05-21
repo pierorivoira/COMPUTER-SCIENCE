57016016c1880f03309f91358436ce1a41fe63dd
Piero Rivoira <52272023+pierorivoira@users.noreply.github.com>
2025-05-21 20:43:12 +0200

    Add files via upload

---
+++ tartaglia_triangolo_modulo.py
@@ -0,0 +1,16 @@
+from tkinter.simpledialog import askinteger
+# n = askinteger('Entry', 'Inserisci il numero di termini da sommare')
+n = 97
+N = n + 3
+print('Il programma restituisce la riga del Triangolo di Tartaglia formata da %d numeri' % N)
+sequenza_iniziale = [1,2,1]
+triangolo = []
+unità = 1
+
+for j in range(n):
+    sequenza_nuova = [1]
+    for i in range(len(sequenza_iniziale)-1):
+        sequenza_nuova.append(sequenza_iniziale[i]+sequenza_iniziale[i+1])  
+    sequenza_nuova.append(unità)
+    sequenza_iniziale = sequenza_nuova
+    triangolo.append(sequenza_nuova)
